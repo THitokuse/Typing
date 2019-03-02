b1d5eb65b49740ed5b756ddd1753588e106254ba
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-03-02 20:50:36 +0900

    スタイルを整える

---
+++ index.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  let currentWord = 'apple'
+  let currentLocation = 0;
+  let score = 0;
+  let miss = 0;
+  let target = document.getElementById('target');
+  let scoreLabel = document.getElementById('score');
+  let missLabel = document.getElementById('miss');
+})();
