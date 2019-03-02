3877e53dc76f3104e87c4b093b02f14dd539c6f7
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-03-02 21:02:54 +0900

    タイプしたキーを取得する

--- index.js
+++ index.js
@@ -8,4 +8,18 @@
   let target = document.getElementById('target');
   let scoreLabel = document.getElementById('score');
   let missLabel = document.getElementById('miss');
+
+  target.innerHTML = currentWord;
+  scoreLabel.innerHTML = score;
+  missLabel.innerHTML = miss;
+
+  window.addEventListener('keyup', function(e) {
+    //e.keyCode
+    //console.log(String.fromCharCode(e.keyCode));
+    if (String.fromCharCode(e.keyCode) === currentWord[currentLocation].toUpperCase()) {
+      //console.log('score!');
+    } else {
+      //console.log('miss!');
+    }
+  });
 })();
